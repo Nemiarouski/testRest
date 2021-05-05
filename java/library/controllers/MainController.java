803283680d847dcc87618840ac4a39393b996367
Peter <70132288+Nemiarouski@users.noreply.github.com>
2021-05-05 16:56:55 +0300

    Refresh project

---
+++ java/library/controllers/MainController.java
@@ -0,0 +1,15 @@
+package library.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello! It's the main page!";
+    }
+
+
+}
